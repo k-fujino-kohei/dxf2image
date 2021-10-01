7ff4608556b5876a3ddbd46b6d9857405ab05d45
Kohei Fujino <appleblueberrycherry@gmail.co.jp>
2021-10-01 15:26:49 +0900

    support line_weight

--- src/dxf2svg.rs
+++ src/dxf2svg.rs
@@ -37,6 +37,7 @@ fn entity_to_node(document: &mut Document, drawing: &dxf::Drawing, coord: &Coord
     }
 
     let color = hex_color(&drawing, &common);
+    let line_weight = coord.relative_to(line_weight(drawing, &common) as f64);
 
     match &entity.specific {
         EntityType::LwPolyline(polyline) => {
@@ -46,14 +47,14 @@ fn entity_to_node(document: &mut Document, drawing: &dxf::Drawing, coord: &Coord
                 .into_iter()
                 .map(|vertex| (coord.relative_to((vertex.x.clone(), vertex.y.clone()))))
                 .collect();
-            let node = Polyline::new().points(points).stroke(&color);
+            let node = Polyline::new().points(points).stroke(&color).stroke_width(line_weight);
             document.add(node);
         }
         EntityType::Line(line) => {
             let line = line.clone();
             let from = coord.relative_to(line.p1);
             let to = coord.relative_to(line.p2);
-            let node = Line::new().points(from, to).stroke(&color);
+            let node = Line::new().points(from, to).stroke(&color).stroke_width(line_weight);
             document.add(node);
         }
         EntityType::Leader(leader) => {
@@ -64,7 +65,7 @@ fn entity_to_node(document: &mut Document, drawing: &dxf::Drawing, coord: &Coord
                 .into_iter()
                 .map(|p| coord.relative_to(p))
                 .collect();
-            let node = Polyline::new().points(points).stroke(&color);
+            let node = Polyline::new().points(points).stroke(&color).stroke_width(line_weight);
             document.add(node);
         }
         EntityType::Circle(circle) => {
@@ -81,7 +82,8 @@ fn entity_to_node(document: &mut Document, drawing: &dxf::Drawing, coord: &Coord
             let radius = coord.relative_to(arc.radius);
             let is_clockwise = drawing.header.angle_direction == AngleDirection::Clockwise;
             let node = Path::arc(center, radius, arc.start_angle, arc.end_angle, is_clockwise)
-                .stroke(&color);
+                .stroke(&color)
+                .stroke_width(line_weight);
             document.add(node);
         }
         EntityType::Insert(insert) => {
@@ -109,4 +111,18 @@ fn hex_color(drawing: &dxf::Drawing, common: &dxf::entities::EntityCommon) -> St
         common.color.index()
     };
     DxfColor::from(color_idx.unwrap_or(0)).as_hex()
+}
+
+fn line_weight(drawing: &dxf::Drawing, common: &dxf::entities::EntityCommon) -> i16 {
+    let by_layer = common.lineweight_enum_value == 0;
+    let line_weight = if by_layer {
+        drawing
+            .layers()
+            .find(|layer| layer.name == common.layer)
+            .map(|layer| layer.line_weight.to_owned().get_raw_value())
+            .unwrap_or_default()
+    } else {
+        common.lineweight_enum_value
+    };
+    line_weight.max(1)
 }
